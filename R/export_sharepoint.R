b3b4b22d2fc4f6a6a110d290b50c03e9a2166567
Francesca Bryden <francesca.bryden@dft.gov.uk>
2025-07-02 14:47:13 +0000

    Set export functions to be more broad

--- R/export_sharepoint.R
+++ R/export_sharepoint.R
@@ -2,21 +2,17 @@
 #' Export an R object as a CSV or XLSX file to a specified Sharepoint location
 #' @name export_sharepoint
 #' @param x data object in R
-#' @param file Name and location of the file you want to save to sharepoint. Includes the extension and any folders after the drive name
 #' @param site Name of sharepoint site you want to save a file to. Can be found in the URL of the Sharepoint location, e.g. for "departmentfortransportuk.sharepoint.com/sites/Rail", the site name would be Rail
 #' @param drive Name of the sharepoint drive you want to save the file to Can be found in the URL of the sharepoint location e.g. for "departmentfortransportuk.sharepoint.com/sites/Rail/RailStats", the drive name is "RailStats". Can also be found using the list_sharepoint_drives() function in this package.
+#' @param file Name and location of the file you want to save to sharepoint. Includes the extension and any folders after the drive name
 #' @export
 #' @importFrom Microsoft365R get_sharepoint_site
 #' @importFrom openxlsx write.xlsx
 #' @importFrom data.table fwrite
 
-export_sharepoint <- function(x, file, site, drive){
+export_sharepoint <- function(x, site, drive, file){
 
-  site_loc <- Microsoft365R::get_sharepoint_site(
-    site_url =
-      paste0("https://departmentfortransportuk.sharepoint.com/sites/", site),
-    auth_type = "device_code")
-
+  site_loc <- get_sharepoint_site(site)
   drive_loc <- site_loc$get_drive(drive)
 
   ##Check your filename has a usable extension
@@ -25,12 +21,26 @@ export_sharepoint <- function(x, file, site, drive){
   }
 
   ##Check your folder exists
-  ##Folder without file name
-  folder <- gsub("(.*[/]).*$", "\\1", file)
-  drive_exist <- try(drive_loc$get_item(folder), silent = TRUE)
+  if(grepl("[/]", file)){
+    ##Folder without file name
+    folder <- gsub("(.*[/]).*$", "\\1", file)
+    drive_exists <- try(drive_loc$get_item(folder), silent = TRUE)
 
-  if( "try-error" %in% class(drive_exist) ) {
-    stop("Folder location ", folder, " not found")
+    ##Check if folder exists, offer to make it if not
+    if("try-error" %in% class(drive_exists) ) {
+      message("Folder location ", folder, " not found")
+      response <- tolower(readline(prompt = "Do you want to create a new folder? (yes/no): "))
+
+      if (response %in% c("yes", "y")) {
+        message("Creating folder: ", folder)
+
+        # Create the folder — assuming drive is defined
+        drive_loc$create_folder(folder)
+        message("Folder created successfully.")
+      } else {
+        stop(folder, " not found and not created")
+      }
+    }
   }
 
   if(grepl("[.]xlsx$", file)){
@@ -58,18 +68,15 @@ export_sharepoint <- function(x, file, site, drive){
 #' Export an R object as a CSV or XLSX file to a specified Sharepoint location
 #' @name export_sharepoint_file
 #' @param x Name and location of a local file.
-#' @param file Name and location on sharepoint you want the file to be saved to. Includes the extension and any folders after the drive name
 #' @param site Name of sharepoint site you want to save a file to. Can be found in the URL of the Sharepoint location, e.g. for "departmentfortransportuk.sharepoint.com/sites/Rail", the site name would be Rail
 #' @param drive Name of the sharepoint drive you want to save the file to Can be found in the URL of the sharepoint location e.g. for "departmentfortransportuk.sharepoint.com/sites/Rail/RailStats", the drive name is "RailStats". Can also be found using the list_sharepoint_drives() function in this package.
+#' @param file Name and location on sharepoint you want the file to be saved to. Includes the extension and any folders after the drive name
 #' @export
 #' @importFrom Microsoft365R get_sharepoint_site
 
-export_sharepoint_file <- function(x, file, site, drive){
+export_sharepoint_file <- function(x, site, drive, file){
 
-  site_loc <- Microsoft365R::get_sharepoint_site(
-    site_url =
-      paste0("https://departmentfortransportuk.sharepoint.com/sites/", site),
-    auth_type = "device_code")
+  site_loc <- get_sharepoint_site(site)
 
   drive_loc <- site_loc$get_drive(drive)
 
@@ -79,17 +86,37 @@ export_sharepoint_file <- function(x, file, site, drive){
   }
 
   ##Check your folder exists
-  ##Folder without file name
-  folder <- gsub("(.*[/]).*$", "\\1", file)
-  drive_exists <- try(drive_loc$get_item(folder), silent = TRUE)
+  ##Skip if no folder
+  if(grepl("[/]", file)){
+    ##Folder without file name
+    folder <- gsub("(.*[/]).*$", "\\1", file)
+    drive_exists <- try(drive_loc$get_item(folder), silent = TRUE)
 
-  if("try-error" %in% class(drive_exists) ) {
-    stop("Folder location ", folder, " not found")
+    ##Check if folder exists, offer to make it if not
+    if("try-error" %in% class(drive_exists) ) {
+      message("Folder location ", folder, " not found")
+      response <- tolower(readline(prompt = "Do you want to create a new folder? (yes/no): "))
+
+      if (response %in% c("yes", "y")) {
+        message("Creating folder: ", folder)
+
+        # Create the folder — assuming drive is defined
+        drive_loc$create_folder(folder)
+        message("Folder created successfully.")
+      } else {
+      stop(folder, " not found and not created")
+      }
+    }
   }
 
   #Save that file on sharepoint
-  drive_loc$upload_file(src = x,
-                        dest = file)
+  tryCatch({
+    drive_loc$upload_file(src = x, dest = file)
+    message("File '", file, "' uploaded successfully")
+    },
+      error = function(e) {
+    stop("Failed to upload file '", file, "': ", e$message)
+  })
 
 }
 
