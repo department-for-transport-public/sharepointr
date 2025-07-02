c52863fef8059300cf0c7d1bc8f86416793db2e1
Francesca Bryden <francesca.bryden@dft.gov.uk>
2025-07-02 15:36:10 +0000

    Added whole folder save

--- R/export_sharepoint.R
+++ R/export_sharepoint.R
@@ -6,7 +6,6 @@
 #' @param drive Name of the sharepoint drive you want to save the file to Can be found in the URL of the sharepoint location e.g. for "departmentfortransportuk.sharepoint.com/sites/Rail/RailStats", the drive name is "RailStats". Can also be found using the list_sharepoint_drives() function in this package.
 #' @param file Name and location of the file you want to save to sharepoint. Includes the extension and any folders after the drive name
 #' @export
-#' @importFrom Microsoft365R get_sharepoint_site
 #' @importFrom openxlsx write.xlsx
 #' @importFrom data.table fwrite
 
@@ -65,14 +64,14 @@ export_sharepoint <- function(x, site, drive, file){
 
 }
 
-#' Export an R object as a CSV or XLSX file to a specified Sharepoint location
+#' Export any file to a specified Sharepoint location
 #' @name export_sharepoint_file
 #' @param x Name and location of a local file.
 #' @param site Name of sharepoint site you want to save a file to. Can be found in the URL of the Sharepoint location, e.g. for "departmentfortransportuk.sharepoint.com/sites/Rail", the site name would be Rail
 #' @param drive Name of the sharepoint drive you want to save the file to Can be found in the URL of the sharepoint location e.g. for "departmentfortransportuk.sharepoint.com/sites/Rail/RailStats", the drive name is "RailStats". Can also be found using the list_sharepoint_drives() function in this package.
 #' @param file Name and location on sharepoint you want the file to be saved to. Includes the extension and any folders after the drive name
 #' @export
-#' @importFrom Microsoft365R get_sharepoint_site
+#' @import Microsoft365R
 
 export_sharepoint_file <- function(x, site, drive, file){
 
@@ -120,3 +119,36 @@ export_sharepoint_file <- function(x, site, drive, file){
 
 }
 
+
+#' Upload a local folder to SharePoint
+#' @name export_sharepoint_folder
+#' @param local_folder Path to the local folder to upload
+#' @param site SharePoint site name (e.g. "Rail")
+#' @param drive SharePoint drive name (e.g. "RailStats")
+#' @param dest_path Destination path in SharePoint drive (e.g. "Reports/July2025"). Defaults to NULL, which will put the folder in the same location as it is locally.
+#' @export
+#' @import Microsoft365R
+
+export_sharepoint_folder <- function(local_folder, site, drive, dest_path = NULL) {
+
+  #If folder is NULL, set to same as local
+  if(is.null(dest_path)){
+    dest_path <- local_folder
+  }
+  # Check local folder exists
+  if (!dir.exists(local_folder)) {
+    stop("Local folder '", local_folder, "' does not exist.")
+  }
+
+  # Get SharePoint location and drive
+  site_loc <- get_sharepoint_site(site)
+  drive_loc <- site_loc$get_drive(drive)
+
+  # Upload folder to destination
+  tryCatch({
+    drive_loc$upload_folder(src = local_folder, dest = dest_path, recursive = TRUE)
+    message("Folder '", local_folder, "' uploaded successfully")
+  }, error = function(e) {
+    stop("Failed to upload folder: ", e$message)
+  })
+}
